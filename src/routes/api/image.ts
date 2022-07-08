e1176404b955210847dedf8cbfad915230056e19
Saif Abuelhasan <saif@DESKTOP-BD63ERL.localdomain>
2022-07-08 22:45:06 +0300

    finished three tests

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,11 +1,84 @@
 import express from 'express';
+import sharp from 'sharp';
 const image = express();
 
+const fullImagesPath = process.cwd() + '/assets/full';
+const thumbImagesPath = process.cwd() + '/assets/thumb';
+
+// variable to store current image data
+type ImageData = {
+	filename: string;
+	width: number;
+	height: number;
+};
+
+let currentImage: ImageData = {
+	filename: '',
+	width: 0,
+	height: 0
+};
+
 /**
- * image API Route
+ * resize image to fit in a box of size width x height
+ * @param {string} filename - image filename
+ * @param {number} width - width of box
+ * @param {number} height - height of box
+ * @returns {ImageData} - image data
  */
-image.get('/', (req, res) => {
-	res.send('image api route!');
+const resizeImage = async (
+	filename: string,
+	width: number,
+	height: number
+): Promise<ImageData> => {
+	const imagePath = `${fullImagesPath}/${filename}.jpg`;
+	const thumbPath = `${thumbImagesPath}/${filename}_thumb.jpg`;
+
+	// if data is different from current image, resize image
+	if (
+		currentImage.filename !== filename ||
+		currentImage.width !== width ||
+		currentImage.height !== height
+	) {
+		console.log(`resizing image ${filename}`);
+		// resize image
+		await sharp(imagePath).resize(width, height).toFile(thumbPath);
+	}
+
+	// throw input file missing error if filename is empty
+	if (filename === '') {
+		throw new Error('input file missing');
+	}
+
+	// return image data
+	return { filename, width, height };
+};
+
+// @route   GET api/images?filename=:filename&width=:width&height=:height
+image.get('/', async (req, res) => {
+	const {
+		filename,
+		width,
+		height
+	}: { filename?: string; width?: string; height?: string } = req.query;
+
+	// set filename to empty string if not provided
+	const filename_ = filename === undefined ? '' : filename;
+
+	// set width and height to 0 if not provided
+	const width_ = width ? +width : 0;
+	const height_ = height ? +height : 0;
+	// try to resize image and catch error if it fails
+	try {
+		currentImage = await resizeImage(filename_, width_, height_);
+		const thumbPath = `${thumbImagesPath}/${filename_}_thumb.jpg`;
+		// respond with image
+		res.sendFile(thumbPath);
+	} catch (err) {
+		if (err instanceof Error) res.status(400).send(err.message);
+	}
+
+	// await resizeImage(filename, w, h);
+	// res.status(200).send('image resized');
 });
 
 export default image;
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -2,25 +2,25 @@ import index from '../index';
 import request from 'supertest';
 
 // test image routes
-describe('image routes tests', () => {
-	it('should return a 200 response', async () => {
+describe('Test endpoint response', () => {
+	it('gets the api/images endpoint', async () => {
 		const res = await request(index).get('/api/images');
-		expect(res.status).toBe(200);
+		expect(res.status).toBe(400);
 	});
 });
 
-// it('should get basic data on the country canada', async () => {
-// 	const data = await countries.getCountry('canada');
-// 	expect(data).toEqual({
-// 		capital: 'Ottawa',
-// 		region: 'Americas',
-// 		numericCode: '124'
-// 	});
-// });
+describe('Test image resize', () => {
+	it('resizes an image successfully', async () => {
+		const res = await request(index).get(
+			'/api/images?filename=fjord&width=100&height=100'
+		);
+		expect(res.status).toBe(200);
+	});
 
-// /** Add test for getRegionCountries function here */
-
-// it('should get capitals of NAFTA countries', async () => {
-// 	const data = await countries.getRegionCapitals('nafta');
-// 	expect(data).toEqual(['Ottawa', 'Mexico City', 'Washington, D.C.']);
-// });
+	it('fails to resize non-existent image', async () => {
+		const res = await request(index).get(
+			'/api/images?filename=test&width=100&height=100'
+		);
+		expect(res.status).toBe(400);
+	});
+});
