62309f37c2edf082898527d5fa25239ef005849b
Saif Abuelhasan <saif@DESKTOP-BD63ERL.localdomain>
2022-07-08 16:19:45 +0300

    endpoint test complete

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,17 +1,26 @@
-import countries from '../index';
+import index from '../index';
+import request from 'supertest';
 
-it('should get basic data on the country canada', async () => {
-	const data = await countries.getCountry('canada');
-	expect(data).toEqual({
-		capital: 'Ottawa',
-		region: 'Americas',
-		numericCode: '124'
+// test image routes
+describe('image routes tests', () => {
+	it('should return a 200 response', async () => {
+		const res = await request(index).get('/api/image');
+		expect(res.status).toBe(200);
 	});
 });
 
-/** Add test for getRegionCountries function here */
+// it('should get basic data on the country canada', async () => {
+// 	const data = await countries.getCountry('canada');
+// 	expect(data).toEqual({
+// 		capital: 'Ottawa',
+// 		region: 'Americas',
+// 		numericCode: '124'
+// 	});
+// });
 
-it('should get capitals of NAFTA countries', async () => {
-	const data = await countries.getRegionCapitals('nafta');
-	expect(data).toEqual(['Ottawa', 'Mexico City', 'Washington, D.C.']);
-});
+// /** Add test for getRegionCountries function here */
+
+// it('should get capitals of NAFTA countries', async () => {
+// 	const data = await countries.getRegionCapitals('nafta');
+// 	expect(data).toEqual(['Ottawa', 'Mexico City', 'Washington, D.C.']);
+// });
